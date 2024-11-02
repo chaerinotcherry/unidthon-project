7666cb1099a74c7fc2f2c7294bbaef3e1269b1c5
katie424 <7080katie@gmail.com>
2024-11-03 07:50:30 +0900

    home 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import Detail from "./detail/Detail";
 import Home from "./home/Home";
 import Mypage from "./mypage/Mypage";
+import Header from "./home/components/Header";
+import Custom from "./home/Custom";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mypage" element={<Mypage />} />{" "}
-        <Route path="/detail" element={<Detail />} />{" "}
-      </Routes>
+      <MainContent />
     </Router>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+  const showHeader = !["/mypage", "/detail/:id"].includes(location.pathname);
+
+  return (
+    <div>
+      <div className="App">
+        {showHeader && <Header />}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mypage" element={<Mypage />} />{" "}
+            <Route path="/detail/:id" element={<Detail />} />{" "}
+            <Route path="/custom" element={<Custom />} />{" "}
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
