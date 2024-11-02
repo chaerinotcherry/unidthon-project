a8269794054ac83897d3078c6bec5f6322d52618
esdx245 <esdx245@khu.ac.kr>
2024-11-03 02:05:23 +0900

    공고 관련 DB 테이블 수정

---
+++ backend/models/gonggoApt.js
@@ -0,0 +1,76 @@
+// models/GonggoApt.js
+const { Sequelize, DataTypes, FLOAT } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const GonggoApt = sequelize.define('GonggoApt', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      },
+    시도: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    시군구: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    주택명: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    평수: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      },
+
+    공급호수: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    보증금: {
+      type: DataTypes.LONG,
+      allowNull: false,
+      },
+    월세: {
+        type: DataTypes.LONG,
+        allowNull: false,
+      },
+    평면도_object_key: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    주소: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    임대기간: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    재계약가능횟수: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    gonggoId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Gonggos', 
+        key: 'id',
+      },
+    },
+  });
+
+    
+GonggoApt.associate = (models) => {
+    GonggoApt.hasMany(models.GonggoApt, {
+      foreignKey: 'gonggoAptId', 
+      as: '주변편의시설', 
+    });
+  };
+    
+  return GonggoApt;
+};
