8503b09ebe559a47e1eeb1c688d87816bc05f431
katie424 <7080katie@gmail.com>
2024-11-03 10:15:25 +0900

    status 수정

--- frontend/src/home/Status.js
+++ frontend/src/home/Status.js
@@ -10,7 +10,7 @@ function Custom() {
     {
       id: 10,
       rank: "예상 1순위",
-      daysLeft: "D+2",
+      daysLeft: "발표완료",
       title: "구로구 매입임대주택",
       price: "월 126,100원 ~",
       location: "서울특별시 구로구",
@@ -19,7 +19,7 @@ function Custom() {
     {
       id: 11,
       rank: "예상 2순위",
-      daysLeft: "D+3",
+      daysLeft: "심사중",
       title: "용산구 매입임대주택",
       price: "월 151,200원 ~",
       location: "서울특별시 용산구",
@@ -28,7 +28,7 @@ function Custom() {
     {
       id: 9,
       rank: "예상 3순위",
-      daysLeft: "D+8",
+      daysLeft: "심사중",
       title: "강동구 매입임대주택",
       price: "월 242,000원 ~",
       location: "서울특별시 강동구",
@@ -39,11 +39,24 @@ function Custom() {
   return (
     <div className="home-page">
       <div className="cus-section">
-        <h2 className="section-title">맞춤 공고</h2>
+        <h2 className="section-title">내가 신청한 청약</h2>
         <div className="card-container">
           {customCards.map((card) => (
             <CustomCard key={card.id} card={card} />
           ))}
+        </div>
+      </div>
+      <div className="cus-section">
+        <h2 className="section-title">청약 알림</h2>
+        <div className="alarm-container">
+          <h5 style={{ fontSize: "15px" }}>
+            유니드 님이 신청한 청약 "구로구 매매임대주택" 결과가 발표되었습니다!
+          </h5>
+        </div>
+        <div className="alarm-container">
+          <h5 style={{ fontSize: "15px" }}>
+            유니드 님이 추가한 청약 "용산구 매입임대주택" 접수가 마감되었습니다!
+          </h5>
         </div>
       </div>
     </div>
